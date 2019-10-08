d81535d17edf5715a5dd14d848694484baf7de30
jiwonchoi <jiwonchoi712@gmail.com>
2019-10-08 20:58:24 +0900

    logout

--- express_example_03/app.js
+++ express_example_03/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login'); //login.js
 var loginProcessRouter = require('./routes/loginProcess'); //우리 프로젝트 내 특정경로
+var logoutRouter = require('./routes/logout');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter); //login.url
 app.use('/loginProcess', loginProcessRouter); //미들웨어에 등록
+app.use('/logout', logoutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- express_example_03/routes/loginProcess.js
+++ express_example_03/routes/loginProcess.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* POST login process */
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     //login.ejs에서 post로 넘기기 때문에!
     //<form id="loginForm" action="/loginProcess" method="post">
-  //res.send('login id= '+req.body.id + ', password= '+req.body.password);
-    if (req.body.login_id=='123456'){
+    //res.send('login id= '+req.body.id + ', password= '+req.body.password);
+    if (req.body.login_id == '123456') {
         req.session.logined = true;
         req.session.login_id = req.body.login_id; //서버에 저장되는 값 = 사용자로부터 넘어오는 값
-    }else{
+    } else {
         req.session.destroy();
     }
     res.redirect('/');  //root로 redirect. app.js의 root찾아서 indexRouter--> index.js 실행
