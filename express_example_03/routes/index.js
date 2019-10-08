9cebde13c77b2195e6eca25ab74abca6783277af
jiwonchoi <jiwonchoi712@gmail.com>
2019-10-08 20:22:01 +0900

    loginProcess and session

--- express_example_03/routes/index.js
+++ express_example_03/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', content:'hello World~!' });
+  if(req.session.logined){
+    res.render('index', { title: 'Express', login_id:req.session.login_id});
+  }else{
+    res.render('index', { title: 'Express', login_id:''});
+  }
 });
 
 module.exports = router;
--- express_example_03/routes/loginProcess.js
+++ express_example_03/routes/loginProcess.js
@@ -5,7 +5,14 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
     //login.ejs에서 post로 넘기기 때문에!
     //<form id="loginForm" action="/loginProcess" method="post">
-  res.send('login id= '+req.body.id + ', password= '+req.body.password);
+  //res.send('login id= '+req.body.id + ', password= '+req.body.password);
+    if (req.body.login_id=='123456'){
+        req.session.logined = true;
+        req.session.login_id = req.body.login_id; //서버에 저장되는 값 = 사용자로부터 넘어오는 값
+    }else{
+        req.session.destroy();
+    }
+    res.redirect('/');  //root로 redirect. app.js의 root찾아서 indexRouter--> index.js 실행
 });
 
 module.exports = router;
